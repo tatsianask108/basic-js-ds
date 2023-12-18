81762eb01ff23a15cde13626d058d42aa493e25a
tatsianask108 <tt.laurie@gmail.com>
2023-12-18 19:10:38 +0300

    feat: solve stack

--- src/queue.js
+++ src/queue.js
@@ -15,22 +15,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Queue {
 
-  queue = [];
-
-
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
   }
 }
 
