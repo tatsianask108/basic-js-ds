d81bf099af9c06cc10b091e55415a7ad21b8ff10
tatsianask108 <tt.laurie@gmail.com>
2023-12-18 15:33:39 +0300

    init: start task

--- src/queue.js
+++ src/queue.js
@@ -15,6 +15,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 class Queue {
 
+  queue = [];
+
+
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
